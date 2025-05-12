1e66e9a94a7d98a3beaae7c4119313f7a5950e49
Mohit Lavania <117350402+mohitlavania07@users.noreply.github.com>
2025-05-12 18:02:25 +0000

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
-echo "Stopping Django service if running..."
-sudo rm -rf /home/ubuntu/django-notes-app
+set -e
+
+echo "Syncing files to destination..."
+DEPLOY_DIR=/home/ubuntu/django-notes-app
+SOURCE_DIR=$CODEDEPLOY_DEPLOYMENT_ROOT/$CODEDEPLOY_DEPLOYMENT_GROUP_ID/deployment-archive
+
+# Create destination directory if not exist
+mkdir -p $DEPLOY_DIR
+
+# rsync to copy only new files, skip existing ones
+sudo rsync -av --ignore-existing $SOURCE_DIR/ $DEPLOY_DIR/
+
 
 
