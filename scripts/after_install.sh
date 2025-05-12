576c83ab7f44d10cad7e000c8eb8685c839695ce
Mohit Lavania <117350402+mohitlavania07@users.noreply.github.com>
2025-05-13 01:16:46 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,16 @@
 echo "[AfterInstall] Activating virtualenv..."
 source /home/ubuntu/django-notes-app/venv/bin/activate
 
-echo "[AfterInstall] Installing dependencies..."
+echo "[AfterInstall] Fixing permissions for staticfiles directory..."
+chown -R ubuntu:ubuntu /home/ubuntu/django-notes-app/staticfiles
+
+echo "[AfterInstall] Installing Python dependencies..."
 pip install -r /home/ubuntu/django-notes-app/requirements.txt
+
+echo "[AfterInstall] Applying migrations..."
+python manage.py migrate
+
+echo "[AfterInstall] Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "[AfterInstall] Done."
