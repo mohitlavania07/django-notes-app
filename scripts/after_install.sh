e12230626057d72748cfe83585376858bc04e4f8
Mohit Lavania <117350402+mohitlavania07@users.noreply.github.com>
2025-05-13 01:24:23 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -5,11 +5,11 @@ set -e
 echo "[AfterInstall] Activating virtualenv..."
 source /home/ubuntu/django-notes-app/venv/bin/activate
 
-echo "[AfterInstall] Fixing ownership of app directory..."
+echo "[AfterInstall] Setting ownership..."
 sudo chown -R ubuntu:ubuntu /home/ubuntu/django-notes-app
 
-echo "[AfterInstall] Fixing ownership of staticfiles directory..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/django-notes-app/staticfiles
+echo "[AfterInstall] Cleaning up existing staticfiles..."
+sudo rm -rf /home/ubuntu/django-notes-app/staticfiles
 
 echo "[AfterInstall] Installing dependencies..."
 pip install -r /home/ubuntu/django-notes-app/requirements.txt
