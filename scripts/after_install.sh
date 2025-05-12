b086628357d6b0be9b1292934c3b30d72d05e08e
Mohit Lavania <117350402+mohitlavania07@users.noreply.github.com>
2025-05-13 01:14:01 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-# Navigate to the project directory
-cd /home/ubuntu/django-notes-app
-
-# Check if virtualenv exists
-if [ ! -d "/home/ubuntu/django-notes-app/venv" ]; then
-  echo "Virtualenv not found. Creating a new one..."
-  python3 -m venv /home/ubuntu/django-notes-app/venv
-fi
-
-# Activate virtual environment
+echo "[AfterInstall] Activating virtualenv..."
 source /home/ubuntu/django-notes-app/venv/bin/activate
 
-echo "[AFTER INSTALL] Installing dependencies..."
+echo "[AfterInstall] Installing dependencies..."
 pip install -r /home/ubuntu/django-notes-app/requirements.txt
-
-echo "[AFTER INSTALL] Applying DB migrations..."
-python manage.py migrate --noinput
-
-echo "[AFTER INSTALL] Collecting static files..."
-python manage.py collectstatic --noinput
