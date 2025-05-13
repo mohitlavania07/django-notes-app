01d72203f7f71477fa941c3c4181650723b8a763
Mohit Lavania <117350402+mohitlavania07@users.noreply.github.com>
2025-05-13 10:22:14 +0000

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,6 +2,11 @@
 echo "[AfterInstall] Activating virtualenv..."
 source /home/ubuntu/django-notes-app/venv/bin/activate
 
+echo "[AfterInstall] Cleaning up old files..."
+sudo rm -rf /home/ubuntu/django-notes-app/buildspec.yml
+sudo rm -rf /home/ubuntu/django-notes-app/scripts
+sudo rm -rf /home/ubuntu/django-notes-app/staticfiles
+
 echo "[AfterInstall] Installing dependencies..."
 pip install -r /home/ubuntu/django-notes-app/requirements.txt
 
