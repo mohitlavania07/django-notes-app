bee3f2b12540fa0b8016a98dc4e5f6fd4c7da6d6
Mohit Lavania <117350402+mohitlavania07@users.noreply.github.com>
2025-05-14 17:28:39 +0000

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
+echo "[AfterInstall] Cleaning up previous temp project if exists..."
+sudo rm -rf /home/ubuntu/django-notes-app-old
+
+echo "[AfterInstall] Backing up current project before overwrite..."
+sudo mv /home/ubuntu/django-notes-app /home/ubuntu/django-notes-app-old
+
+echo "[AfterInstall] Copying new files to main project folder..."
+sudo cp -r /home/ubuntu/temp-django-notes-app /home/ubuntu/django-notes-app
+
+echo "[AfterInstall] Setting permissions..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/django-notes-app
+
 echo "[AfterInstall] Activating virtualenv..."
 source /home/ubuntu/django-notes-app/venv/bin/activate
-
-echo "[AfterInstall] Cleaning up old files..."
-sudo rm -rf /home/ubuntu/django-notes-app/buildspec.yml
-sudo rm -rf /home/ubuntu/django-notes-app/scripts
-sudo rm -rf /home/ubuntu/django-notes-app/staticfiles
 
 echo "[AfterInstall] Installing dependencies..."
 pip install -r /home/ubuntu/django-notes-app/requirements.txt
@@ -13,15 +21,9 @@ pip install -r /home/ubuntu/django-notes-app/requirements.txt
 echo "[AfterInstall] Collecting static files..."
 python /home/ubuntu/django-notes-app/manage.py collectstatic --noinput
 
-echo "[AfterInstall] Copying files from temp directory to original destination..."
-sudo cp -r /home/ubuntu/temp-django-notes-app/* /home/ubuntu/django-notes-app/
-
-echo "[AfterInstall] Setting permissions for static files..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/django-notes-app/staticfiles
-
- sudo systemctl daemon-reload
- 
-echo "[AfterInstall] Restarting Django service..."
+echo "[AfterInstall] Restarting Django & Nginx..."
+sudo systemctl daemon-reload
 sudo systemctl restart django.service
-
 sudo systemctl restart nginx
+
+echo "[AfterInstall] Deployment completed successfully!"
